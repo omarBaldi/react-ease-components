16ff36fbc21824488baa3bbaf45748903b23541f
Omar Baldi <omar.baldi@maersk.com>
2023-07-26 19:27:32 +0200

    Added optional prop to render fallback element when condition is false

--- src/components/Show.tsx
+++ src/components/Show.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 type ShowProps = {
   when: boolean;
   children: React.ReactNode;
+  fallbackElement?: React.ReactNode;
 };
 
-export default function Show({ when: shouldRenderChildren, children }: ShowProps) {
-  return <React.Fragment>{shouldRenderChildren && children}</React.Fragment>;
+export default function Show({
+  when: shouldRenderChildren,
+  children,
+  fallbackElement,
+}: ShowProps) {
+  return shouldRenderChildren
+    ? children
+    : fallbackElement ?? <React.Fragment></React.Fragment>;
 }
