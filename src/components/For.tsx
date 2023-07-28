2d479a9eb88a1a469d5b9889a63973b24ff07556
Omar Baldi <omar.baldi@maersk.com>
2023-07-28 15:28:38 +0200

    Refactoring of For custom component to render react function components or native html elements using discriminated union

--- src/components/For.tsx
+++ src/components/For.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 
-type ForProps<T> = {
+/**
+ * @description
+ * Prevent from passing other elements rather than objects
+ */
+type ComponentForProps<T extends object> = {
+  __type: 'ComponentForProps';
   each: T[];
   element: React.FunctionComponent<T>;
 };
 
-export default function For<T>({ each, element: ElementToRender }: ForProps<T>) {
-  return each.map((el, index) => <ElementToRender key={index} {...el} />);
+type NativeElement = {
+  __type: 'NativeElement';
+  each: string[];
+  element: keyof JSX.IntrinsicElements;
+};
+
+function isNativeElement<T extends object>(
+  props: ComponentForProps<T> | NativeElement
+): props is NativeElement {
+  return props.__type === 'NativeElement';
+}
+
+export default function For<T extends object>(
+  props: ComponentForProps<T> | NativeElement
+) {
+  return isNativeElement(props)
+    ? props.each.map((el, index) => {
+        return React.createElement(props.element, {
+          key: index,
+          children: el,
+        });
+      })
+    : props.each.map((el, index) => {
+        return <props.element key={index} {...el} />;
+      });
 }
