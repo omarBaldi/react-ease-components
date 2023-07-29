4cbeae0f9cb9e120b751268ed95d8dd9a23bf15d
Omar Baldi <omar.baldi@maersk.com>
2023-07-29 12:53:53 +0200

    Added optional fallback element to render if no conditions match

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 interface Case<T> {
   condition: T;
   elementToRender: React.FunctionComponent;
@@ -6,12 +8,19 @@ interface Case<T> {
 type SwitchProps<T> = {
   conditionToEvaluate: T;
   cases: Case<T>[];
+  fallbackElement?: React.ReactNode;
 };
 
-export default function Switch<T>({ conditionToEvaluate, cases }: SwitchProps<T>) {
+export default function Switch<T>({
+  conditionToEvaluate,
+  cases,
+  fallbackElement,
+}: SwitchProps<T>) {
   const conditionFound = cases.find(({ condition }) => condition === conditionToEvaluate);
 
-  return (
-    <>{typeof conditionFound !== 'undefined' && <conditionFound.elementToRender />}</>
+  return typeof conditionFound !== 'undefined' ? (
+    <conditionFound.elementToRender />
+  ) : (
+    fallbackElement ?? <React.Fragment></React.Fragment>
   );
 }
